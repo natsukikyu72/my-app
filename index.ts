e64865f8efb42c7ba5496b280cfadeac43f5b0a9
natsukikyu72 <natsukikyu72@keio.jp>
2026-06-18 01:26:25 +0900

    Update for Render deployment

---
+++ index.ts
@@ -0,0 +1,36 @@
+import "dotenv/config";
+import express from "express";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "./generated/prisma/client";
+
+// データベース接続の準備
+const pool = new Pool({ connectionString: process.env.DATABASE_URL,ssl: { rejectUnauthorized: false } });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter, log: ["query"] });
+
+const app = express();
+const PORT = process.env.PORT || 8888;
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(express.urlencoded({ extended: true }));
+
+// ユーザー一覧を表示する
+app.get("/", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.render("index", { users });
+});
+
+// 新しいユーザーを追加する
+app.post("/users", async (req, res) => {
+  const name = req.body.name;
+  if (name) {
+    await prisma.user.create({ data: { name } });
+  }
+  res.redirect("/");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
